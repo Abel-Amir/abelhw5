d2d24d5872f3aae498346ba75f2922749c93dd2b
Абель-Амир <kubanichbekuuluabelamir@gmail.com>
2022-01-03 21:50:58 +0600

    hw5

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import get_books, book_detail, add_book
+from .views import get_books, book_detail, add_book, BookDetailView, BookListView, BookCreateView
 
 urlpatterns = [
-    path("books/", get_books, name = "book_view"),
-    path("books/<int:id>/", book_detail, name ="book_detail_view"),
-    path("add-book/", add_book, name = "add_book_view")
+    path('books/', BookListView.as_view()),
+    path('books/<int:pk>/', BookDetailView.as_view()),
+    path('add-book/', BookCreateView.as_view())
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -2,6 +2,8 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.generic import ListView, DetailView, CreateView
+
 from .forms import BookForm, CommentForm
 from .models import Book, Comment
 from django.shortcuts import render
@@ -9,6 +11,11 @@ def get_books(request):
     books = Book.objects.all()
 
     return render(request, "book_list.html", {'books': books})
+
+class BookListView(ListView):
+    queryset = Book.objects.all()
+    template_name = "book_list.html"
+    context_object_name = "books"
 
 
 def book_detail(request, id):
@@ -26,6 +33,38 @@ def book_detail(request, id):
 
     return render(request, 'book_detail.html', {'book': book, 'comments': comments, 'comment_form': form})
 
+class BookDetailView(DetailView):
+    queryset = Book.objects.all()
+    template_name = 'book_detail.html'
+    context_object_name = 'book'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book_id = self.kwargs['pk']
+        comments = Comment.objects.filter(book_id=book_id)
+        context['comments'] = comments
+        context['comment_form'] = CommentForm
+
+
+        return context
+
+    def post(self,request, *args, **kwargs):
+        print("Hellow world")
+
+        print('here ja;sdfjlasdkjf')
+        form = CommentForm(request.POST)
+        self.object = self.get_object()
+        context = super().context_object_name(**kwargs)
+        print('not here')
+        if form.is_valid():
+            Comment.objects.create(book_id=id, comment_text=form.data.get('comment_text'))
+        else:
+            form = CommentForm()
+        context['comment_form'] = form
+
+
+
+        return self.render_to_response(context=context)
 
 def add_book(request):
     method = request.method
@@ -41,3 +80,9 @@ def add_book(request):
         form = BookForm
 
     return render(request, 'add_book.html', {'form': form})
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'add_book.html'
+    form_class = BookForm
+    success_url = '/books/'
